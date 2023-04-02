aed6fbba83063290bad01551738546763dbd8eb8
Diwash <diwashdahal75@gmail.com>
2023-04-02 21:29:06 +0545

    chore: remove build folder GitHub

--- out/_next/static/chunks/app/layout-59ea492d1e786d69.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1115:function(n,e,u){Promise.resolve().then(u.t.bind(u,4026,23))},4026:function(){}},function(n){n.O(0,[667,961,744],function(){return n(n.s=1115)}),_N_E=n.O()}]);
