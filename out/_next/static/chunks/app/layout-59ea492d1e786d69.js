3fc5f76312e6c34c1080e6002b9195a9ecf957a9
Diwash <diwashdahal75@gmail.com>
2023-04-02 20:48:54 +0545

    add: static exports

---
+++ out/_next/static/chunks/app/layout-59ea492d1e786d69.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1115:function(n,e,u){Promise.resolve().then(u.t.bind(u,4026,23))},4026:function(){}},function(n){n.O(0,[667,961,744],function(){return n(n.s=1115)}),_N_E=n.O()}]);
