c65eb281341cef94dbccb572b6fbf2831968f321
Diwash <diwashdahal75@gmail.com>
2023-04-02 20:46:54 +0545

    fix: not able to export static files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: '.next',
+  assetPrefix: './',
   experimental: {
     appDir: true,
   },
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
